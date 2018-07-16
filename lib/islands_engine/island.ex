58c4934fa676be8501d11c3887f4a6f8a6c22db9
ug0 <ug600@icloud.com>
2018-07-16 14:33:34 +0800

    Guess island miss or hit

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -14,6 +14,16 @@ defmodule IslandsEngine.Island do
     end
   end
 
+  def guess(%Island{} = island, %Coordinate{} = coordinate) do
+    case MapSet.member?(island.coordinates, coordinate) do
+      true ->
+        hit_coordinates = MapSet.put(island.hit_coordinates, coordinate)
+        {:hit, %{island | hit_coordinates: hit_coordinates}}
+      false ->
+        :miss
+    end
+  end
+
   def overlaps?(existing_island, new_island) do
     not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
   end
--- test/islands_engine/island_test.exs
+++ test/islands_engine/island_test.exs
@@ -15,4 +15,14 @@ defmodule IslandsEngine.IslandTest do
     assert Island.overlaps?(square, dot)
     refute Island.overlaps?(square, l_shape)
   end
+
+  test "guess a island hit or miss" do
+    {:ok, hit_coordinate} = Coordinate.new(1, 1)
+    {:ok, island} = Island.new(:square, hit_coordinate)
+    {:ok, miss_coordinate} = Coordinate.new(3, 3)
+
+    assert :miss = Island.guess(island, miss_coordinate)
+    assert {:hit, updated_island} = Island.guess(island, hit_coordinate)
+    assert MapSet.member?(updated_island.hit_coordinates, hit_coordinate)
+  end
 end
